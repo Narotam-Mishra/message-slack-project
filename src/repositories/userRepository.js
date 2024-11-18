6337ef079df30eebe0820d22bca41c2281fa628e
Narotam-Mishra <narotam21jan@gmail.com>
2024-11-18 14:36:30 +0530

    message_slack_project_added_repository_layer_1.8

---
+++ src/repositories/userRepository.js
@@ -0,0 +1,15 @@
+
+
+export const getUserByEmail = (email) => {};
+
+export const getUserByName = (name) => {};
+
+export const createUser = (user) => {}
+
+export const getUsers = () => {};
+
+export const getUserById = () => {};
+
+export const deleteUser = (id) => {};
+
+export const updateUser = (id, user) => {};
