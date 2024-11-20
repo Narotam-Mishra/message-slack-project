e0c50634a645e1296c4f29b66452984ec9013ce1
Narotam-Mishra <narotam21jan@gmail.com>
2024-11-20 23:16:47 +0530

    message_slack_project_added_signin_service_1.12

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,6 +1,10 @@
-import userRepository from "../repositories/userRepository.js";
-import ValidationError from "../utils/errors/validationError.js";
+import bcrypt from 'bcrypt';
+import { StatusCodes } from "http-status-codes";
 
+import userRepository from "../repositories/userRepository.js";
+import { createJWT } from '../utils/common/authUtils.js';
+import ClientError from "../utils/errors/clientError.js";
+import ValidationError from "../utils/errors/validationError.js";
 
 export const signUpService = async (data) => {
     try {
@@ -21,3 +25,41 @@ export const signUpService = async (data) => {
         }
     }
 };
+
+export const signInService = async (data) => {
+    try {
+        const user = await userRepository.getByEmail(data.email);
+        if(!user){
+            throw new ClientError({
+                explanation: 'Invalid data sent from the client',
+                message: 'No registered user found with this email',
+                statusCode: StatusCodes.NOT_FOUND
+            });
+        }
+
+        // match the incoming password with hashed password
+        const isMatch = bcrypt.compareSync(data.password, user.password);
+
+        if(!isMatch){
+            throw new ClientError({
+                explanation: 'Invalid data sent from the client',
+                message: 'Invalid password, please try again',
+                statusCode: StatusCodes.BAD_REQUEST
+            });
+        }
+
+        // if password matched then return necessary details in response
+        return{
+            username: user.username,
+            email: user.email,
+            avatar: user.avatar,
+            token: createJWT({
+                id: user._id,
+                email: user.email
+            })
+        };
+    } catch (error) {
+        console.log(`User service error: ${error}`);
+        
+    }
+}
