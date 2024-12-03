037c969941f29735f5346a262ac16a5e3187577c
Narotam-Mishra <narotam21jan@gmail.com>
2024-12-03 23:04:34 +0530

    message_slack_project_added_workspace_service_1.19_p1

---
+++ src/services/workspaceService.js
@@ -0,0 +1,22 @@
+import { v4 as uuidv4 } from 'uuid';
+
+import workspaceRepository from "../repositories/workspaceRepository"
+
+export const createWorkspaceService = async (workspaceData) => {
+    const joinCode = uuidv4().substring(0, 6);
+    const response = await workspaceRepository.create({
+      name: workspaceData.name,
+      description: workspaceData.description,
+      joinCode
+    });
+
+    await workspaceRepository.addMemberToWorkspace(
+      response._id,
+      workspaceData.owner,
+      'admin'
+    );
+
+    await workspaceRepository.addChannelToWorkspace(response._id, 'general');
+
+    return response;
+}
